3f69dbda2a48a2bc12904b1da0e3544c469e8ecc
Enzo Amá Fatobene <enzoamafatobene@gmail.com>
2026-05-26 12:25:45 -0300

    Criacao Repositorio

---
+++ src/main/java/br/com/fiap/challenge/ouro/OuroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.ouro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuroApplication.class, args);
+	}
+
+}
